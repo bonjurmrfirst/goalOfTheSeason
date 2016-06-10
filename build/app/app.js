b259231d99e43e6f84a7b8159f5de17b860075c6
Ilya <bonjurmrfirst@gmail.com>
2016-06-10 12:17:36 +0300

    initial commit

---
+++ build/app/app.js
@@ -0,0 +1,96 @@
+'use strict';
+
+angular
+  .module('playerOfTheYear', [])
+
+  .controller('MainController', ['$scope', 'playersService', function($scope, playersService) {
+    $scope.players = playersService.players;
+
+    $scope.send = function(player) {
+      var localStorageKey = 'bestPlayerOfTheSeason04';
+
+      if (!store.get(localStorageKey)) {
+        swal({
+            title: "Лучший игрока сезона: \n" + player.name + ' ' + player.surname,
+            text: "Вы уверены?",
+            type: "info",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Да, отправить!",
+            cancelButtonText: "Нет, вернуться к выбору игроков",
+            closeOnConfirm: false,
+            closeOnCancel: true
+          },
+          function (isConfirm) {
+            if (isConfirm) {
+              swal("Отправлено!", "Итоги голосования будут опубликованы на официальном сайте, следите за новостями!", "success");
+              store.set(localStorageKey, player.name + ' ' + player.surname);
+            } else {
+              //swal("Отменено!", "", "error");
+            }
+          });
+      } else {
+        swal({
+          title: "Ваш голос учтен!",
+          text: "Вы выбрали: " + store.get(localStorageKey),
+          type: "info",
+          showCancelButton: false,
+          confirmButtonColor: "#DD6B55",
+          confirmButtonText: "Закрыть",
+          closeOnConfirm: true
+        });
+      }
+    };
+
+  }])
+
+  .factory('playersService', function() {
+    var players = [
+      {
+        name: 'Алекс',
+        surname: 'Тейшейра',
+        month: 'Лучший в июле, сентябре, октябре, ноябре, декабре',
+        count: 5,
+        src: 'Teixeira'
+      },
+      {
+        name: '',
+        surname: 'Марлос',
+        month: 'Лучший в августе, апреле',
+        count: 2,
+        src: 'Marlos'
+      },
+      {
+        name: 'Виктор',
+        surname: 'Коваленко',
+        month: 'Лучший в январе',
+        count: 1,
+        src: 'Kovalenko'
+      },
+      {
+        name: 'Андрей',
+        surname: 'Пятов',
+        month: 'Лучший в феврале',
+        count: 1,
+        src: 'Pyatov'
+      },
+      {
+        name: '',
+        surname: 'Тайсон',
+        month: 'Лучший в марте',
+        count: 1,
+        src: 'Tayson'
+      },
+      {
+        name: '',
+        surname: 'Эдуардо',
+        month: 'Лучший в мае',
+        count: 1,
+        src: 'Eduardo'
+      }
+    ];
+
+    return {
+      players: players
+    }
+  });
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp'),
+  del = require('del'),
+  autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('pre-build', function() {
+  del(['./build/**/*.*', '!./build/index.html']);
+
+  gulp.src(['development/**/*.*', '!development/index.html'])
+    .pipe(gulp.dest('build'));
+
+  gulp.src([
+    'bower_components/angular/angular.min.js',
+    'bower_components/sweetalert/dist/sweetalert.min.js'
+  ])
+    .pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('build', function() {
+  gulp.src('build/assets/css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['>0.01%'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('build/assets/css'));
+});
